32ac5d879a5ce47fd3fa51218a3610d9e92b7ef2
debajyoti <debajyoti94@gmail.com>
2021-01-15 16:16:14 +0530

    Defined unit test cases.

---
+++ src/test_functionalities.py
@@ -0,0 +1,34 @@
+'''In this code we will create unit tests for checking data sanity and also
+unit tests for the functions that we create in this project.'''
+
+class TestFunctionalities:
+
+    def test_null_checks(self):
+        '''
+        column null checks
+        :return: True if all is okay
+        '''
+        return
+
+    def test_data_shape_check(self):
+        '''
+        check if the shape of the original data matches
+         with what is expected
+        :return: True if all is okay
+        '''
+        return
+
+    def test_train_data_check(self):
+        '''
+        check if the train and test data exists
+        :return: True if all is okay
+        '''
+        return
+
+    def test_delimiter_check(self):
+        '''
+        check if the delimiter of the file matches
+         with what is expected
+        :return: True if all is okay
+        '''
+        return
