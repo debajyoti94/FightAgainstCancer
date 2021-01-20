0ca03b65688199deb04f0d991f762cfca9f10027
debajyoti <debajyoti94@gmail.com>
2021-01-20 22:22:03 +0530

    Added code for unit tests.

--- src/test_functionalities.py
+++ src/test_functionalities.py
@@ -1,6 +1,13 @@
 '''In this code we will create unit tests for checking data sanity and also
 unit tests for the functions that we create in this project.'''
 
+import config
+import feature_engg
+
+import csv
+import os
+import pandas as pd
+
 class TestFunctionalities:
 
     def test_null_checks(self):
@@ -8,7 +15,13 @@ class TestFunctionalities:
         column null checks
         :return: True if all is okay
         '''
-        return
+        dl_obj = feature_engg.DumpLoadFile()
+        train_set = dl_obj.load_file(config.CLEAN_TRAIN_FILENAME)
+        test_set = dl_obj.load_file(config.CLEAN_TEST_FILENAME)
+
+        assert False if  False in pd.isnull(train_set) or \
+                       False in pd.isnull(test_set) \
+                       else True
 
     def test_data_shape_check(self):
         '''
@@ -16,14 +29,18 @@ class TestFunctionalities:
          with what is expected
         :return: True if all is okay
         '''
-        return
+        dataset = pd.read_csv(config.ORIGINAL_DATASET_FILENAME)
+        assert True if dataset.shape == config.DATASET_SHAPE else False
+
 
     def test_train_data_check(self):
         '''
         check if the train and test data exists
         :return: True if all is okay
         '''
-        return
+        assert True if os.path.isfile(config.CLEAN_TRAIN_FILENAME) \
+                       and os.path.isfile(config.CLEAN_TEST_FILENAME)\
+                       else False
 
     def test_delimiter_check(self):
         '''
@@ -31,4 +48,8 @@ class TestFunctionalities:
          with what is expected
         :return: True if all is okay
         '''
-        return
+        with open(config.ORIGINAL_DATASET_FILENAME, "r") as csv_file:
+            file_contents = csv.Sniffer().sniff(csv_file.readline())
+
+        assert True if file_contents.delimiter == config.FILE_DELIMITER \
+                                                            else False
